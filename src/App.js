206f1d010010a2e7369f1c09591cfda7eb3a49b7
Balint Ligeti <balint.ligeti@hotmail.com>
2021-02-02 10:48:39 +0100

    bootstrap config

--- src/App.js
+++ src/App.js
@@ -1,62 +1,16 @@
-import React, { Component, useState } from "react"
+import React, { Component, useState, useEffect } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import axios from "axios"
+import Game from "./1medium/Game"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-    const [count, setCount] = useState();
-  }
+function App() {
 
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const data = res.data;
-        this.setCount({ data });
-      })
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
 
     return (
-      <p>
-        <p>Joker Child</p>
-        <p>{count.data}</p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+      <Game />
     )
-  }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
