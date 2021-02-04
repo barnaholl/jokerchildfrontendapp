01c3c1c0ad24d156ade4fc345feb65da614522f7
barnaholl <hollbarna@freemail.hu>
2021-02-04 12:15:46 +0100

    PurpleBurron onClick

--- src/1big/GetId.jsx
+++ src/1big/GetId.jsx
@@ -7,11 +7,16 @@ import urhajoshata from "../pics/urhajoshata.png"
 
 
 export default function GetId() {
+
+    const test = () => {
+        console.log("works");
+    }
+
     return (
         <div>
             <PinkInfo text="Kérlek add meg a kártyán található kódot amivel játszani szeretnél." />
             <input type="text" name="name"  className="input1"/>
-            <PurpleButton text="Mutasd a kártyát!" />
+            <PurpleButton text="Mutasd a kártyát!" onClick={test}/>
             <div className="pics">
                 <img src={urhajos} style={{height: "320px", paddingRight:"1%"}}/>
                 <img src={urhajoshata} style={{height: "320px"}}/>
--- src/1small/PurpleButton.jsx
+++ src/1small/PurpleButton.jsx
@@ -4,9 +4,11 @@ import {Button, Card} from 'react-bootstrap';
 
 
 export default function PurpleButton(props) {
+
+
     return (
         <div className="purpleButt">
-            <Button variant="default" style={{color: 'white', background: '#7749f8', fontFamily:'sans-serif', fontWeight:"bolder"}}>{props.text}</Button>
+            <Button variant="default" onClick={props.onClick} style={{color: 'white', background: '#7749f8', fontFamily:'sans-serif', fontWeight:"bolder"}}>{props.text}</Button>
         </div>
     )
 }
