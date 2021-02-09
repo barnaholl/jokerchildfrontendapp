2b66128c9f2b0fd2a79f8b8a2201b4ee8963a4a6
Balint Ligeti <balint.ligeti@hotmail.com>
2021-02-09 14:36:37 +0100

    netlify deploy

--- src/1big/WelcomePage.jsx
+++ src/1big/WelcomePage.jsx
@@ -6,14 +6,20 @@ import "../game.css"
 import PinkInfo from "../1medium/PinkInfo"
 import PurpleButton from "../1small/PurpleButton"
 import MiniCard from "../1small/MiniCard"
+import GetId from "./GetId";
+import { useHistory } from 'react-router-dom'
 
 
 export default function WelcomePage() {
+    const history=useHistory(); 
+    const getNewCard=()=>{
+        history.push("/GetId");
+    }
     return (
         <div>
             <PinkInfo title="Joker Child" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " />
             <div className="purplButt">
-                <PurpleButton text="Belépek!" />
+            <PurpleButton onClick={getNewCard} text="Kód beírás" />
             </div>
                 <MiniCard text="Diákként " text2="regisztrálok!" align="left" />
                 <MiniCard text="Tanárként " text2="regisztrálok!" align="right" />
