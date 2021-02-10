e22124cd83b6fbdc81d614c6bc8c879c68b43852
Balint Ligeti <balint.ligeti@hotmail.com>
2021-02-10 18:19:21 +0100

    drag&drop works

--- src/1big/Answer.jsx
+++ src/1big/Answer.jsx
@@ -7,6 +7,8 @@ import urhajoshata from "../pics/urhajoshata.png"
 import felho from "../pics/felho.png"
 import nap from "../pics/nap.png"
 import "./getId.css"
+import "./answer.css"
+
 import "./questions.css"
 import { CardContext, AnswerIdContext } from '../context/CardContext'
 import { useHistory } from 'react-router-dom'
@@ -17,22 +19,43 @@ export default function DndTest() {
 
     const context = useContext(CardContext);
     const answerIdContext = useContext(AnswerIdContext)
-    const [words, setWords] = useState([])
     let goodWords = [];
 
 
-
-// fake data generator
-const getItems = (count) =>
-    Array.from({ length: count }, (v,k) => k).map(k => ({
-        id: `item-${k}`,
-        content: `item ${k}`
-    }));
+    useEffect( () => {
+        setAnswers();
+    }, [])
+    const [question,setQuestion] = useState('')
+    const setAnswers = () => {
+        let allWords = context.card.exercises[answerIdContext.answerId].answer.split(";");
+        goodWords = allWords[0].split(",");
+        let badWords = allWords[1].split(",");
+        const newW = goodWords.concat(badWords);
+        setQuestion(context.card.exercises[answerIdContext.answerId].question);
+        setState( () => {
+            return {
+                words: newW,
+                items: getItems(newW.length, newW),
+                selected: getItems(0),
+            }
+        })
+    }
 
     const [state,setState] = useState({
-        items: getItems(3),
-        selected: getItems(0)
+        words: [],
+        items: [],
+        selected: [],
+        
     })
+
+    // fake data generator
+    const getItems = (count, w) =>
+        Array.from({ length: count }, (v,k) => k).map(k => ({
+            id: `item-${k}`,
+            content: w[k]
+        }));
+
+
 
     // a little function to help us with reordering the result
     const reorder = (list, startIndex, endIndex) => {
@@ -60,19 +83,19 @@ const getItems = (count) =>
     const getItemStyle = (isDragging, draggableStyle) => ({
         // some basic styles to make the items look a bit nicer
         userSelect: 'none',
-        padding: grid * 2,
         margin: `0 0 ${grid}px 0`,
-
+        borderRadius: '20px',
         // change background colour if dragging
         background: isDragging ? 'lightgreen' : 'grey',
-
+        height: '35px',
         // styles we need to apply on draggables
         ...draggableStyle
     });
 
     const getListStyle = isDraggingOver => ({
         padding: grid,
-        width: 250
+        width: 250,
+        height: '100%',
     });
 
 
@@ -133,11 +156,18 @@ const getItems = (count) =>
     // But in this example everything is just done in one place for simplicity
     return (
         <DragDropContext onDragEnd={onDragEnd}>
+            <PinkInfo text={question}/>
+            <div className="inline">
+            
             <Droppable droppableId="droppable">
                 {(provided, snapshot) => (
                     <div
+                        className="firstDroppable"
                         ref={provided.innerRef}
                         style={getListStyle(snapshot.isDraggingOver)}>
+                        <div>
+                <p>Válasz lehetőségek:</p>
+            </div>
                         {state.items.map((item, index) => (
                             <Draggable
                                 key={item.id}
@@ -161,11 +191,17 @@ const getItems = (count) =>
                     </div>
                 )}
             </Droppable>
+            </div>
+            <div className="inline">
             <Droppable droppableId="droppable2">
                 {(provided, snapshot) => (
                     <div
+                        className="secondDroppable"
                         ref={provided.innerRef}
                         style={getListStyle(snapshot.isDraggingOver)}>
+                            <div>
+                <p>Ide húzd a válaszaid:</p>
+            </div>
                         {state.selected.map((item, index) => (
                             <Draggable
                                 key={item.id}
@@ -189,6 +225,8 @@ const getItems = (count) =>
                     </div>
                 )}
             </Droppable>
+        </div>
+        <PurpleButton text="Kész!" />
         </DragDropContext>
     );
 }
